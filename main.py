67d595c75aae1ba2ef67e8f5ba39cadb56d21287
Vann4 <1vetlan1@mail.ru>
2023-09-20 17:36:06 +0300

    Изменение импорта библиотеки tkinter

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 #Калькулятор
-import tkinter as tk
+from tkinter import *
 def button_click(number):
     current = entry.get()
-    entry.delete(0, tk.END)
-    entry.insert(tk.END, current + str(number))
+    entry.delete(0, END)
+    entry.insert(END, current + str(number))
 
 def button_clear():
-    entry.delete(0, tk.END)
+    entry.delete(0, END)
 
 def button_equal():
     try:
         expression = entry.get()
         result = eval(expression)
-        entry.delete(0, tk.END)
-        entry.insert(tk.END, result)
+        entry.delete(0, END)
+        entry.insert(END, result)
     except:
-        entry.delete(0, tk.END)
-        entry.insert(tk.END, "Error")
+        entry.delete(0, END)
+        entry.insert(END, "Error")
 
-root = tk.Tk()
+root = Tk()
 root.title("Calculator")
 root.geometry('223x269+800+300')
 root.resizable (width=False, height=False)
 
-entry = tk.Entry(root, width=25, borderwidth=5, background="#000000", fg="#00FF00", font="Arial 10")
+entry = Entry(root, width=25, borderwidth=5, background="#000000", fg="#00FF00", font="Arial 10")
 entry.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
 buttons = [
@@ -46,13 +46,13 @@ buttons = [
 
 for button_data in buttons:
     button_text, row, column = button_data
-    button = tk.Button(root, text=button_text, padx=20, pady=10, background="#CCCCFF", width=1, command=lambda button_text=button_text: button_click(button_text))
+    button = Button(root, text=button_text, padx=20, pady=10, background="#CCCCFF", width=1, command=lambda button_text=button_text: button_click(button_text))
     button.grid(row=row, column=column)
 
-clear_button = tk.Button(root, text="C", padx=20, pady=10, background="#FF0033", width=1, command=button_clear)
+clear_button = Button(root, text="C", padx=20, pady=10, background="#FF0033", width=1, command=button_clear)
 clear_button.grid(row=5, column=0)
 
-equal_button = tk.Button(root, text="=", padx=20, pady=10, background="#CCCCFF", command=button_equal)
+equal_button = Button(root, text="=", padx=20, pady=10, background="#CCCCFF", command=button_equal)
 equal_button.grid(row=5, column=2)
 
 root.mainloop()
